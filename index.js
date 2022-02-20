040e7ff7e9538c931b2cae40236cecb8fafad51f
Ben Grant <benjamin.j.grant@gmail.com>
2022-02-20 13:15:50 -0800

    Detect pa6 changes, not pa7

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ async function update() {
 		$ = cheerio.load(html);
 
 	const lines = $('a[href=pa7.pdf]').parent().text().split('\n'),
-		pa7Index = lines.findIndex(l => l.includes('pa7')),
-		endIndex = lines.findIndex((l, i) => i > pa7Index && l.includes('pa8')),
+		pa7Index = lines.findIndex(l => l.includes('pa6')),
+		endIndex = lines.findIndex((l, i) => i > pa7Index && l.includes('pa7')),
 		relevantLines = lines.slice(pa7Index, endIndex).filter(l => !l.match(/^\W*$/)),
 		final = relevantLines.map(l => l.replaceAll(/^\W+|\W+$/g, '')).join('\n');
 
